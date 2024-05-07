72ecf6a8c38fd17fb1680b5ed52243b9cf183daf
Will Kinderman <github.com@defective.computer>
2024-05-07 15:05:58 -0700

    Fix spacing in azure

--- sephiroth/providers/azure.py
+++ sephiroth/providers/azure.py
@@ -14,9 +14,9 @@ class Azure(BaseProvider):
         azure_download_page = (
             "https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519"
         )
-         headers = {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/112.0'
-                }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/112.0'
+        }
         r = requests.get(azure_download_page, headers = headers)
         soup = BeautifulSoup(r.content, "html.parser")
         direct_link = soup.select_one(".failoverLink")["href"]
