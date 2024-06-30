cf8f389780fe64f94826a26e59c9b70867752518
0xdade <0xdade@users.noreply.github.com>
2024-06-30 15:58:05 -0700

    Fix classifiers

--- setup.py
+++ setup.py
@@ -28,7 +28,10 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9" "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
     ],
     python_requires=">=3.6",
 )
